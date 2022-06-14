5ede16eb2b4c8ad0221be3b69e602b986b572830
KimDongGa <dgkim0419@gmail.com>
2022-06-15 02:46:48 +0900

    Initial commit

---
+++ src/main/java/com/myty/project/SpringBootDgApplication.java
@@ -0,0 +1,13 @@
+package com.myty.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDgApplication.class, args);
+	}
+
+}
